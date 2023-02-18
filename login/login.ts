045a7ab5fbc47a83e58e97fd9d1a90f02d9c04ef
Orekarako <orekarako@gmail.com>
2023-02-18 12:56:11 +0200

    f

---
+++ login/login.ts
@@ -0,0 +1,29 @@
+function checkMatchUserDetails(emailUser: string, passwordUser: string): boolean{
+    try {
+
+    } catch (error) {
+        console.error(error);
+        return false
+    }
+}
+function checksIfUserExists(emailUser: string): boolean{
+    try {
+
+    } catch (error) {
+        console.error(error);
+        return false
+
+    }
+}
+
+function handleSubmitLogIn(ev: any) {
+    try {
+        ev.preventDefault()
+        const { email, password } = ev.target.elements
+
+    }
+
+    catch (error) {
+        console.error(error);
+    }
+}
